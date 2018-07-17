99388f6636a28e1f6d17f54865034baae99ecf25
abhrp <abhiroop.patel@gmail.com>
2018-07-17 17:50:04 +0530

    Minor changes in ui

--- remote/src/main/java/com/github/abhrp/remote/constants/MoviesDbConstants.kt
+++ remote/src/main/java/com/github/abhrp/remote/constants/MoviesDbConstants.kt
@@ -3,4 +3,5 @@ package com.github.abhrp.remote.constants
 object MoviesDbConstants {
     const val API_KEY = ""
     const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 }
