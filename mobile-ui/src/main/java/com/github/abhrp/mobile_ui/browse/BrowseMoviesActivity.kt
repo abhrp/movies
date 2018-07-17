b3ebb285a1454d871e2f9c9d8a412063e47666b2
abhrp <abhiroop.patel@gmail.com>
2018-07-17 10:34:10 +0530

    Added shortlist movies adapter

--- mobile-ui/src/main/java/com/github/abhrp/mobile_ui/browse/BrowseMoviesActivity.kt
+++ mobile-ui/src/main/java/com/github/abhrp/mobile_ui/browse/BrowseMoviesActivity.kt
@@ -1,20 +1,48 @@
 package com.github.abhrp.mobile_ui.browse
 
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import com.github.abhrp.mobile_ui.R
+import com.github.abhrp.mobile_ui.di.ViewModelFactory
+import com.github.abhrp.mobile_ui.mapper.MovieMapper
+import com.github.abhrp.presentation.viewmodel.BrowseMoviesViewModel
+import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_browse_movies.*
+import javax.inject.Inject
 
 class BrowseMoviesActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var adapter: BrowseMoviesAdapter
+    @Inject
+    lateinit var mapper: MovieMapper
+    @Inject
+    lateinit var viemodelFactory: ViewModelFactory
+    @Inject
+    lateinit var viewmodel: BrowseMoviesViewModel
+
+    companion object {
+        fun initActivity(context: Context): Intent {
+            return Intent(context, BrowseMoviesActivity::class.java)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AndroidInjection.inject(this)
+
+        viewmodel = ViewModelProviders.of(this, viemodelFactory).get(BrowseMoviesViewModel::class.java)
+
         setContentView(R.layout.activity_browse_movies)
         setUpMoviesListView()
     }
 
     private fun setUpMoviesListView() {
         moviesListView.layoutManager = LinearLayoutManager(this)
+        moviesListView.adapter = adapter
     }
 }
---
+++ mobile-ui/src/main/java/com/github/abhrp/mobile_ui/shortlist/ShortlistedMoviesAdapter.kt
@@ -0,0 +1,39 @@
+package com.github.abhrp.mobile_ui.shortlist
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.abhrp.mobile_ui.R
+import com.github.abhrp.mobile_ui.model.Movie
+import com.squareup.picasso.Picasso
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_shortlisted_movie.*
+import javax.inject.Inject
+
+class ShortlistedMoviesAdapter @Inject constructor() : RecyclerView.Adapter<ShortlistedMoviesAdapter.ViewHolder>() {
+
+    var movies: List<Movie> = arrayListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return movies.count()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val movie = movies[position]
+        holder.bind(movie)
+    }
+
+    inner class ViewHolder(override val containerView: View?) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+        fun bind(movie: Movie) {
+            movieName1.text = movie.title
+            moviePopularity1.text = movie.popularity.toString()
+            Picasso.get().load(movie.posterPath).placeholder(R.drawable.ic_placeholder).into(moviePoster1)
+        }
+    }
+}
